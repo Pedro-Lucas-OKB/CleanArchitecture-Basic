5f38570e302e93dc919e7c002883be15f04b2813
Pedro Lucas <pedrogmobile2@gmail.com>
2024-04-14 14:07:46 -0300

    initial commit

---
+++ CleanArch/CleanArch.Infra.Data/DependencyInjection.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CleanArch.Infra.Data.Context;
+
+namespace CleanArch.Infra.Data;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add services to the container.
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(connectionString,
+            b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName
+        )));
+
+        services.AddDatabaseDeveloperPageExceptionFilter();
+        
+        //Console.WriteLine("Conecction string: " + connectionString);
+        services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        return services;
+    }
+}
